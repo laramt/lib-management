b345443a4599669fb5d7d2208c4b4b09321cbd52
Lara Menck <lara.menckt@gmail.com>
2023-04-14 18:40:23 -0300

    Hold devolution PUT request

--- src/main/java/com/project/librarysystem/controllers/HoldController.java
+++ src/main/java/com/project/librarysystem/controllers/HoldController.java
@@ -2,13 +2,16 @@ package com.project.librarysystem.controllers;
 
 import com.project.librarysystem.models.Book;
 import com.project.librarysystem.models.Hold;
+import com.project.librarysystem.models.Patron;
 import com.project.librarysystem.services.HoldService;
+import com.project.librarysystem.services.PatronService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -17,11 +20,24 @@ public class HoldController {
 
     @Autowired
     HoldService holdService;
+    @Autowired
+    PatronService patronService;
 
     @PostMapping("/{patronId}/{bookId}")
     public ResponseEntity<Object> checkout(@PathVariable(value = "patronId") UUID patronId,
                                            @PathVariable(value = "bookId") UUID bookId){
         return ResponseEntity.status(HttpStatus.CREATED).body(holdService.checkout(patronId, bookId));
+    }
+
+    @PutMapping("devolution/{id}")
+    public ResponseEntity<Object> devolution(@PathVariable(value = "id") UUID id){
+
+        Optional<Hold> hd = holdService.findById(id);
+        if (!hd.isPresent()){
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("Hold does not exist");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(holdService.devolution(id));
     }
 
     @GetMapping("/all")
