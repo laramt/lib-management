3ca4aedc7dd7bfbfe2c646fe73e8e2407a3ec745
Lara Menck <lara.menckt@gmail.com>
2023-05-11 08:36:53 -0300

    isBookAvailable query

--- src/main/java/com/project/librarysystem/repositories/BookRepository.java
+++ src/main/java/com/project/librarysystem/repositories/BookRepository.java
@@ -2,10 +2,14 @@ package com.project.librarysystem.repositories;
 
 import com.project.librarysystem.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("SELECT CASE WHEN (b.status = AVAILABLE) THEN TRUE ELSE FALSE END FROM Book b WHERE b.id = :bookId")
+    boolean isBookAvailable(@Param("bookId") Long bookId);
 
 }
