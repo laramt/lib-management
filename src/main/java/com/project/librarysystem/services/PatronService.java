67ed926f257b3506c0321afca2192e655a86624a
Lara Menck <lara.menckt@gmail.com>
2023-05-11 10:54:00 -0300

    register method refactored

--- src/main/java/com/project/librarysystem/services/PatronService.java
+++ src/main/java/com/project/librarysystem/services/PatronService.java
@@ -4,6 +4,7 @@ import com.project.librarysystem.models.Patron;
 import com.project.librarysystem.repositories.PatronRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,44 +18,13 @@ public class PatronService {
 
     @Autowired
     PatronRepository patronRepository;
+    private static final int MINIMUM_AGE = 16;
 
+    @Transactional
     public Patron registerNewPatron(Patron patron){
-
-        // email validation
-        String email = patron.getEmail();
-        if (email == null || email.isEmpty()){
-            throw new RuntimeException("Email cannot be null or empty.");
-        }
-
-        Pattern patternEmail = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
-        Matcher matcherEmail = patternEmail.matcher(email);
-        if (!matcherEmail.matches()) {
-            throw new RuntimeException("Invalid email.");
-        }
-
-        //phone number validation
-        String phone = patron.getPhoneNumber();
-        if (phone == null || phone.isEmpty()){
-            throw new RuntimeException("Phone number cannot be null or empty");
-        }
-
-        Pattern patternPhone = Pattern.compile("^\\d{13}$");
-        Matcher matcherPhone = patternPhone.matcher(phone);
-        if (!matcherPhone.matches()) {
-            throw new RuntimeException("Invalid phone number.");
-        }
-
-        //birthdate validation
-        LocalDate birthdate = patron.getBirthDate();
-        if (birthdate == null || birthdate.toString().isEmpty()){
-            throw new RuntimeException("Birthdate cannot be null or empty");
-        }
-
-        LocalDate today = LocalDate.now();
-        LocalDate sixteenYearsAgo = today.minusYears(16);
-        if (patron.getBirthDate().isBefore(sixteenYearsAgo)){
-            throw new RuntimeException("Patron must be at least 16 years old.");
-        }
+        validateEmail(patron.getEmail());
+        validatePhoneNumber(patron.getPhoneNumber());
+        validateBirthDate(patron.getBirthDate());
 
         return patronRepository.save(patron);
     }
@@ -65,6 +35,46 @@ public class PatronService {
 
     public Optional<Patron> findById(Long id){
         return patronRepository.findById(id);
+    }
+
+    private void validateEmail(String email) {
+
+        if (email == null || email.isEmpty()){
+            throw new RuntimeException("Email cannot be null or empty.");
+        }
+
+        Pattern patternEmail = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+        Matcher matcherEmail = patternEmail.matcher(email);
+        if (!matcherEmail.matches()) {
+            throw new RuntimeException("Invalid email.");
+        }
+
+    }
+
+    private void validatePhoneNumber(String phoneNumber) {
+
+        if (phoneNumber == null || phoneNumber.isEmpty()){
+            throw new RuntimeException("Phone number cannot be null or empty");
+        }
+
+        Pattern patternPhone = Pattern.compile("^\\d{13}$");
+        Matcher matcherPhone = patternPhone.matcher(phoneNumber);
+        if (!matcherPhone.matches()) {
+            throw new RuntimeException("Invalid phone number.");
+        }
+
+    }
+
+    private void validateBirthDate(LocalDate birthDate) {
+        if (birthDate == null || birthDate.toString().isEmpty()){
+            throw new RuntimeException("Birthdate cannot be null or empty");
+        }
+
+        LocalDate today = LocalDate.now();
+        LocalDate minimumAgeDate = today.minusYears(MINIMUM_AGE);
+        if (birthDate.isAfter(minimumAgeDate)){
+            throw new RuntimeException("Patron must be at least " + MINIMUM_AGE + " years old.");
+        }
     }
 
 }
