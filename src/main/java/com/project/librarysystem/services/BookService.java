97a57d1e7c5eabd12c237e053f668437b872b670
Lara Menck <lara.menckt@gmail.com>
2023-05-18 23:28:15 -0300

    BookServiceImpl

--- src/main/java/com/project/librarysystem/services/BookService.java
+++ src/main/java/com/project/librarysystem/services/BookService.java
@@ -1,48 +1,15 @@
 package com.project.librarysystem.services;
 
-import com.project.librarysystem.exceptions.ResourceNotFoundException;
 import com.project.librarysystem.models.Book;
-import com.project.librarysystem.repositories.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class BookService{
+public interface BookService {
 
-    @Autowired
-    BookRepository bookRepository;
-
-    public Book save(Book book){
-        return bookRepository.save(book);
-    }
-
-    public List<Book> findAll(){
-        return bookRepository.findAll();
-    }
-
-    public Book findById (Long id){
-        Optional<Book> pt = bookRepository.findById(id);
-        Book book = pt.orElseThrow(() -> new ResourceNotFoundException("Book with id " + id + " not found."));
-        return book;
-
-    }
-
-    public void delete (Long id){
-        findById(id);
-        bookRepository.deleteById(id);
-    }
-
-    public Book update (Long id, Book book){
-        Book bk = findById(id);
-
-        bk.setTitle(book.getTitle());
-        bk.setAuthor(book.getAuthor());
-
-        bookRepository.save(bk);
-        return bk;
-    }
+    Book save(Book book);
+    List<Book> findAll();
+    Book findById (Long id);
+    void delete (Long id);
+    Book update (Long id, Book book);
 
 }
---
+++ src/main/java/com/project/librarysystem/services/impl/BookServiceImpl.java
@@ -0,0 +1,49 @@
+package com.project.librarysystem.services.impl;
+
+import com.project.librarysystem.exceptions.ResourceNotFoundException;
+import com.project.librarysystem.models.Book;
+import com.project.librarysystem.repositories.BookRepository;
+import com.project.librarysystem.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    public Book save(Book book){
+        return bookRepository.save(book);
+    }
+
+    public List<Book> findAll(){
+        return bookRepository.findAll();
+    }
+
+    public Book findById (Long id){
+        Optional<Book> pt = bookRepository.findById(id);
+        Book book = pt.orElseThrow(() -> new ResourceNotFoundException("Book with id " + id + " not found."));
+        return book;
+
+    }
+
+    public void delete (Long id){
+        findById(id);
+        bookRepository.deleteById(id);
+    }
+
+    public Book update (Long id, Book book){
+        Book bk = findById(id);
+
+        bk.setTitle(book.getTitle());
+        bk.setAuthor(book.getAuthor());
+
+        bookRepository.save(bk);
+        return bk;
+    }
+
+}
