f2dca93792232c1b776d94f174f0d604d38c7cb2
Lara Menck <lara.menckt@gmail.com>
2023-05-22 18:37:12 -0300

    Book changed to BookDTO

--- src/main/java/com/project/librarysystem/controllers/BookController.java
+++ src/main/java/com/project/librarysystem/controllers/BookController.java
@@ -1,5 +1,6 @@
 package com.project.librarysystem.controllers;
 
+import com.project.librarysystem.dtos.BookDTO;
 import com.project.librarysystem.models.Book;
 import com.project.librarysystem.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +18,16 @@ public class BookController {
     @Autowired
     BookService bookService;
 
-    @PostMapping("/new-book")
-    public ResponseEntity<Book> saveBook(@RequestBody Book book){
-        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.save(book));
-    }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Book>> getAllBooks(){
-        List<Book> list = bookService.findAll();
+    public ResponseEntity<List<BookDTO>> getAllBooks(){
+        List<BookDTO> list = bookService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getBookById(@PathVariable Long id){
-        Book book = bookService.findById(id);
+        BookDTO book = bookService.findById(id);
         if(!Optional.of(book).isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book does not exists");
         }
@@ -38,26 +35,5 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.OK).body(book);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteBookById(@PathVariable Long id){
-        Book book = bookService.findById(id);
-        if(!Optional.of(book).isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book does not exists");
-        }
-
-        bookService.delete(id);
-        return ResponseEntity.status(HttpStatus.OK).body("Book deleted");
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateBook(@PathVariable Long id, @RequestBody Book book){
-        Book bk = bookService.findById(id);
-        if(!Optional.of(bk).isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book does not exists");
-        }
-
-        Book updatedBook = bookService.update(id, book);
-        return ResponseEntity.status(HttpStatus.OK).body(updatedBook);
-    }
 
 }
