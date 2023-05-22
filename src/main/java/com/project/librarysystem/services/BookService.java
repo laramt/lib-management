9923abf25d044759ea389cbaa8ecfa35e558aa13
Lara Menck <lara.menckt@gmail.com>
2023-05-22 18:53:15 -0300

    Book to BookDTO

--- src/main/java/com/project/librarysystem/services/BookService.java
+++ src/main/java/com/project/librarysystem/services/BookService.java
@@ -1,15 +1,13 @@
 package com.project.librarysystem.services;
 
+import com.project.librarysystem.dtos.BookDTO;
 import com.project.librarysystem.models.Book;
 
 import java.util.List;
 
 public interface BookService {
 
-    Book save(Book book);
-    List<Book> findAll();
-    Book findById (Long id);
-    void delete (Long id);
-    Book update (Long id, Book book);
+    List<BookDTO> findAll();
+    BookDTO findById (Long id);
 
 }
