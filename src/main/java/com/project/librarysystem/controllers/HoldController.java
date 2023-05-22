c2a6228d1a68752578698d26fb844f261886518a
Lara Menck <lara.menckt@gmail.com>
2023-05-22 18:48:05 -0300

    Hold to HoldDTO

--- src/main/java/com/project/librarysystem/controllers/HoldController.java
+++ src/main/java/com/project/librarysystem/controllers/HoldController.java
@@ -1,6 +1,6 @@
 package com.project.librarysystem.controllers;
 
-import com.project.librarysystem.models.Hold;
+import com.project.librarysystem.dtos.HoldDTO;
 import com.project.librarysystem.services.HoldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,31 +20,31 @@ public class HoldController {
     @PostMapping("/{patronId}/{bookId}")
     public ResponseEntity<Object> checkout(@PathVariable(value = "patronId") Long patronId,
                                            @PathVariable(value = "bookId") Long bookCopyId){
-        Hold hold = holdService.checkout(patronId, bookCopyId);
+        HoldDTO hold = holdService.checkout(patronId, bookCopyId);
         return ResponseEntity.status(HttpStatus.CREATED).body(hold);
     }
 
     @PutMapping("/devolution/{id}")
     public ResponseEntity<Object> devolution(@PathVariable(value = "id") Long id){
-        Hold hd = holdService.findById(id);
+        HoldDTO hd = holdService.findById(id);
         if (!Optional.of(hd).isPresent()){
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body("Hold with id " + id + " not found.");
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("HoldDTO with id " + id + " not found.");
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(holdService.devolution(id));
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Hold>> getAllHold(){
-        List<Hold> list = holdService.findAll();
+    public ResponseEntity<List<HoldDTO>> getAllHoldDTO(){
+        List<HoldDTO> list = holdService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> getHoldById(@PathVariable Long id){
-        Hold hold = holdService.findById(id);
+    public ResponseEntity<Object> getHoldDTOById(@PathVariable Long id){
+        HoldDTO hold = holdService.findById(id);
         if (!Optional.of(hold).isPresent()){
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body("Hold with id " + id + " not found.");
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("HoldDTO with id " + id + " not found.");
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(hold);
