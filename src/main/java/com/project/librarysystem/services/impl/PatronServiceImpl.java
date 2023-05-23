555b221e846c204e123a228425d91c828fb87314
Lara Menck <lara.menckt@gmail.com>
2023-05-23 11:12:25 -0300

    Patron mapper

--- src/main/java/com/project/librarysystem/services/impl/PatronServiceImpl.java
+++ src/main/java/com/project/librarysystem/services/impl/PatronServiceImpl.java
@@ -1,59 +1,68 @@
 package com.project.librarysystem.services.impl;
 
+import com.project.librarysystem.dtos.PatronDTO;
 import com.project.librarysystem.exceptions.InvalidInputException;
 import com.project.librarysystem.exceptions.ResourceNotFoundException;
+import com.project.librarysystem.mappers.PatronMapper;
 import com.project.librarysystem.models.Patron;
 import com.project.librarysystem.repositories.PatronRepository;
 import com.project.librarysystem.services.PatronService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
+@RequiredArgsConstructor
 public class PatronServiceImpl implements PatronService {
 
-    @Autowired
-    PatronRepository patronRepository;
+    private final PatronRepository patronRepository;
+    private final PatronMapper mapper;
     private static final int MINIMUM_AGE = 16;
 
     @Transactional
-    public Patron registerNewPatron(Patron patron){
-        validatePatronData(patron.getEmail(), patron.getPhoneNumber(), patron.getBirthDate());
-        return patronRepository.save(patron);
+    public PatronDTO registerNewPatron(PatronDTO dto) {
+        validatePatronData(dto.getEmail(), dto.getPhoneNumber(), dto.getBirthDate());
+        patronRepository.save(mapper.toPatron(dto));
+        return dto;
+
     }
 
-    public List<Patron> findAll(){
-        return patronRepository.findAll();
+    public List<PatronDTO> findAll() {
+        return mapper.toPatronDTOList(patronRepository.findAll());
     }
 
-    public Patron findById(Long id){
-        Optional<Patron> pt = patronRepository.findById(id);
-        Patron patron = pt.orElseThrow(() -> new ResourceNotFoundException("Patron with id " + id + " not found."));
-        return patron;
+    public PatronDTO findById(Long id) {
+        Patron patron = patronRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Patron with id " + id + " not found."));
+
+        return mapper.toPatronDTO(patron);
+
     }
 
     @Transactional
-    public Patron update(Long id, Patron patron){
-        Patron pt = findById(id);
-        validatePatronData(patron.getEmail(), patron.getPhoneNumber(), patron.getBirthDate());
+    public PatronDTO update(Long id, PatronDTO dto) {
+        Patron patron = patronRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Patron with id " + id + " not found."));
 
-        pt.setName(patron.getName());
-        pt.setEmail(patron.getEmail());
-        pt.setPhoneNumber(patron.getPhoneNumber());
-        pt.setBirthDate(patron.getBirthDate());
+        validatePatronData(dto.getEmail(), dto.getPhoneNumber(), dto.getBirthDate());
 
-        return patronRepository.save(pt);
+        patron.setName(dto.getName());
+        patron.setEmail(dto.getEmail());
+        patron.setPhoneNumber(dto.getPhoneNumber());
+        patron.setBirthDate(dto.getBirthDate());
+
+        patronRepository.save(patron);
+        return mapper.toPatronDTO(patron);
     }
 
-    private void validatePatronData(String email, String phoneNumber, LocalDate birthDate){
+    private void validatePatronData(String email, String phoneNumber, LocalDate birthDate) {
 
-        if (email == null || email.isEmpty()){
+        if (email == null || email.isEmpty()) {
             throw new InvalidInputException("Email cannot be null or empty.");
         }
 
@@ -63,7 +72,7 @@ public class PatronServiceImpl implements PatronService {
             throw new InvalidInputException("Invalid email.");
         }
 
-        if (phoneNumber == null || phoneNumber.isEmpty()){
+        if (phoneNumber == null || phoneNumber.isEmpty()) {
             throw new InvalidInputException("Phone number cannot be null or empty");
         }
 
@@ -73,13 +82,13 @@ public class PatronServiceImpl implements PatronService {
             throw new InvalidInputException("Invalid phone number.");
         }
 
-        if (birthDate == null || birthDate.toString().isEmpty()){
+        if (birthDate == null || birthDate.toString().isEmpty()) {
             throw new InvalidInputException("Birthdate cannot be null or empty");
         }
 
         LocalDate today = LocalDate.now();
         LocalDate minimumAgeDate = today.minusYears(MINIMUM_AGE);
-        if (birthDate.isAfter(minimumAgeDate)){
+        if (birthDate.isAfter(minimumAgeDate)) {
             throw new InvalidInputException("Patron must be at least " + MINIMUM_AGE + " years old.");
         }
 
