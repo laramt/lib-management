72dc3b77fa67c561af6ed41051dd20b9769676a8
Lara Menck <lara.menckt@gmail.com>
2023-05-31 09:30:50 -0300

    custom query for late book

--- src/main/java/com/project/librarysystem/domain/repositories/HoldRepository.java
+++ src/main/java/com/project/librarysystem/domain/repositories/HoldRepository.java
@@ -4,6 +4,12 @@ import com.project.librarysystem.domain.models.Hold;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface HoldRepository extends JpaRepository<Hold, Long> {
+
+    List<Hold> findByDueDateBeforeAndReturnedIsFalse(LocalDate currentDate);
+
 }
