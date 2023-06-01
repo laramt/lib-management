3aaad9ec130365ef4230cac801fba99b861ea6b3
Lara Menck <lara.menckt@gmail.com>
2023-06-01 18:32:19 -0300

    existsByTitleAndAuthor query and book refactored

--- src/main/java/com/project/librarysystem/repositories/BookRepository.java
+++ src/main/java/com/project/librarysystem/repositories/BookRepository.java
@@ -7,5 +7,6 @@ import com.project.librarysystem.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByTitleAndAuthor(String title, String author);
+    boolean existsByTitleAndAuthor(String title, String author);
 
 }
