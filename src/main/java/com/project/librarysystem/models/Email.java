8df4372d14782042a9f51293a606fbeba3211171
Lara Menck <lara.menckt@gmail.com>
2023-06-01 12:01:31 -0300

    column annotation

--- src/main/java/com/project/librarysystem/models/Email.java
+++ src/main/java/com/project/librarysystem/models/Email.java
@@ -17,19 +17,22 @@ public class Email {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String emailFrom;
+    @Column(nullable = false)
     private String emailTo;
     @ManyToOne
     @JoinColumn(name = "patron_id")
     private Patron patron;
+    @Column(nullable = false)
     private String subject;
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String text;
-    private LocalDateTime sendDateEmail;
+    private LocalDateTime createdAt;
 
     @PrePersist
     protected void onCreate() {
-        sendDateEmail = LocalDateTime.now();
+        createdAt = LocalDateTime.now();
     }
     
 }
