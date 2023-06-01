6b51c8ac159f65418813a204c435a9b82632b9cd
Lara Menck <lara.menckt@gmail.com>
2023-06-01 12:14:13 -0300

    Book model refactored, createdAt and updatedAt

--- src/main/java/com/project/librarysystem/models/Book.java
+++ src/main/java/com/project/librarysystem/models/Book.java
@@ -4,23 +4,49 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
 import java.util.*;
 
 @Entity
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Table(name = "BOOK_TB")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String author;
+
     @Column(columnDefinition = "TEXT")
     private String description;
+
     @JsonIgnore
     @OneToMany(mappedBy = "book")
     private List<BookCopy> copies = new ArrayList<>();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
 
 }
