532e1b0f3d99ef853918ffeb6dfbca805c10c97a
Lara Menck <lara.menckt@gmail.com>
2023-06-01 16:16:53 -0300

    publisher changed from String to Publisher

--- src/main/java/com/project/librarysystem/dtos/BookCopyDTO.java
+++ src/main/java/com/project/librarysystem/dtos/BookCopyDTO.java
@@ -1,6 +1,8 @@
 package com.project.librarysystem.dtos;
 
+
 import com.project.librarysystem.models.Book;
+import com.project.librarysystem.models.Publisher;
 import com.project.librarysystem.models.enums.BookStatus;
 
 import lombok.Data;
@@ -11,7 +13,7 @@ public class BookCopyDTO {
     private Long id;
     private Book book;
     private String isbn;
-    private String publisher;
+    private Publisher publisher;
     private int yearPublished;
     private BookStatus status;
 
