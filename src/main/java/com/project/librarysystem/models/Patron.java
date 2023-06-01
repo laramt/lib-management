3d900eddb5bee93a51139b7c89e086d8997d919e
Lara Menck <lara.menckt@gmail.com>
2023-06-01 12:16:14 -0300

    Patron model refactored, createdAt and updatedAt

--- src/main/java/com/project/librarysystem/models/Patron.java
+++ src/main/java/com/project/librarysystem/models/Patron.java
@@ -5,26 +5,59 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Table(name = "PATRON_TB")
 public class Patron {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String cardNumber;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String phoneNumber;
+
     @JsonFormat(pattern = "dd-MM-yyyy")
+    @Column(nullable = false)
     private LocalDate birthDate;
+
     @JsonIgnore
     @OneToMany(mappedBy = "patron")
     private List<Hold> holds = new ArrayList<>();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
 
 }
