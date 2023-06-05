eaf8cb2235150d6173e07c2f33aaae353c5e8705
Lara Menck <lara.menckt@gmail.com>
2023-06-05 09:24:56 -0300

    AuthorDTO created

---
+++ src/main/java/com/project/librarysystem/dtos/AuthorDTO.java
@@ -0,0 +1,13 @@
+package com.project.librarysystem.dtos;
+
+import lombok.Data;
+
+@Data
+public class AuthorDTO {
+ 
+    private Long id;
+    private String name;
+    private String nationality;
+    private String description;
+    
+}
