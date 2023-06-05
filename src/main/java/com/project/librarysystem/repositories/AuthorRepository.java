706623333cfb2d4d0b80228eca2804d38f0fe4fe
Lara Menck <lara.menckt@gmail.com>
2023-06-05 09:23:04 -0300

    Author repository

---
+++ src/main/java/com/project/librarysystem/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.project.librarysystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.librarysystem.models.Author;
+
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    
+}
