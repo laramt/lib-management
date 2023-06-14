10f474c54b7b003392d2286c2360eee9f4bf512b
Lara Menck <lara.menckt@gmail.com>
2023-06-14 15:37:03 -0300

    Book and Author association

--- src/main/java/com/project/librarysystem/models/Author.java
+++ src/main/java/com/project/librarysystem/models/Author.java
@@ -1,12 +1,18 @@
 package com.project.librarysystem.models;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -36,6 +42,10 @@ public class Author {
 
     @Column(columnDefinition = "TEXT")
     private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books = new ArrayList<>();
     
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant createdAt;
