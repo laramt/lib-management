f28cf11090b804d00a235b8adb009d7d9fd1672c
Lara Menck <lara.menckt@gmail.com>
2023-06-14 15:36:13 -0300

    data seeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,18 @@
-INSERT INTO book_tb (title, author) VALUES ('Água Viva', 'Clarice Lispector');
-INSERT INTO book_tb (title, author) VALUES ('A Metamorfose', 'Franz Kafka');
-INSERT INTO book_tb (title, author) VALUES ('O Lugar', 'Annie Ernaux');
-INSERT INTO book_tb (title, author) VALUES ('Normal People', 'Sally Rooney');
+INSERT INTO author_tb (name, nationality, description) VALUES ('Clarice Lispector', 'Brasil', 'orem ipsum dolor sit amet, consectetur adipiscing elit.');
+INSERT INTO author_tb (name, nationality, description) VALUES ('Franz Kafka', 'Brasil', 'orem ipsum dolor sit amet, consectetur adipiscing elit.');
+INSERT INTO author_tb (name, nationality, description) VALUES ('Annie Ernaux', 'Brasil', 'orem ipsum dolor sit amet, consectetur adipiscing elit.');
+INSERT INTO author_tb (name, nationality, description) VALUES ('Sally Rooney', 'Brasil', 'orem ipsum dolor sit amet, consectetur adipiscing elit.');
+
 
 INSERT INTO publisher_tb (name) VALUES ('Rocco');
 INSERT INTO publisher_tb (name) VALUES ('Antofagica');
 INSERT INTO publisher_tb (name) VALUES ('Penguin Classicos');
+
+
+INSERT INTO book_tb (title, author_id) VALUES ('Agua Viva', 1L);
+INSERT INTO book_tb (title, author_id) VALUES ('A Metamorfose', 2L);
+INSERT INTO book_tb (title, author_id) VALUES ('O Lugar', 3L);
+INSERT INTO book_tb (title, author_id) VALUES ('Normal People', 4L);
 
 
 INSERT INTO book_copy_tb (book_id, isbn, publisher_id, year_published, status) VALUES (1L, '9786555320213', 1L, '2020','CHECKED_OUT');
@@ -13,8 +20,6 @@ INSERT INTO book_copy_tb (book_id, isbn, publisher_id, year_published, status) V
 INSERT INTO book_copy_tb (book_id, isbn, publisher_id, year_published, status) VALUES (2L, '9726485210008', 3L, '2003','AVAILABLE');
 INSERT INTO book_copy_tb (book_id, isbn, publisher_id, year_published, status) VALUES (3L,'9786589733027', 1L, '2021','AVAILABLE');
 INSERT INTO book_copy_tb (book_id, isbn, publisher_id, year_published, status) VALUES (4L, '9781984822178', 2L, '2019','AVAILABLE');
-
-
 
 
 INSERT INTO patron_tb (card_number, name, email, phone_number, birth_date) VALUES ('202312345678', 'Anne Doe', 'anne@email.com', '5543999533937', '1999-07-14');
